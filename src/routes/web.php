60d86019c91ccb43ad1ad80d8b88a2d5c2c00554
Shamshad Zaheer <shamshadtech101@gmail.com>
2024-07-20 17:25:21 -0700

    fix: route bug

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,4 +1,5 @@
 <?php
 use codehubcare\AdminUi\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('admin', [AdminController::class, 'index'])->name('admin.index')
