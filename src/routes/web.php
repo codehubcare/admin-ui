a75b66f68bbced5abede734813a7fca4326498d5
Shamshad Zaheer <shamshadtech101@gmail.com>
2024-07-20 17:28:07 -0700

    fix: Importing Admin controller to the web routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use codehubcare\AdminUi\Http\Controllers\AdminController;
+use Codehubcare\AdminUi\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('admin', [AdminController::class, 'index'])->name('admin.index');
